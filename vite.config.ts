d549a28946f88c80fa215c9f52210c85b8b41cc7
jaacool <mail@jaa.cool>
2025-11-08 12:12:08 +0100

    v1.1.1: Add version badge in header (top-left)

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+      json: {
+        stringify: false
+      },
       define: {
         'process.env.API_KEY': JSON.stringify(env.API_KEY || env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.API_KEY || env.GEMINI_API_KEY),
